8026b74a7c34953b9f208bd1a47aa2fc6b69b3bb
v.yakovlev <wwevovancik@gmail.com>
2022-01-22 16:20:39 +0300

    Первым коммит

---
+++ Frontend/Frontend/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Frontend.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Frontend.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
