ca1d8b88f1d898f75d1076a842c46b9ae8daf468
v.yakovlev <wwevovancik@gmail.com>
2022-02-16 14:42:32 +0300

    Добавлен класс пользователя

---
+++ AirplaneDelivery/DAL/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DAL.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
