91e8f7dae483b905d8fc268f7d64c42c8526f39f
v.yakovlev <wwevovancik@gmail.com>
2022-02-16 22:03:35 +0300

    Добавлен Уровень доступа к данным

--- AirplaneDelivery/DAL/Models/User.cs
+++ AirplaneDelivery/DAL/Models/User.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 namespace DAL.Models
 {
     public class User
     {
-        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Password { get; set; }
