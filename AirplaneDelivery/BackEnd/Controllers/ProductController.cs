b32654da82ddff7389ccd4d150f7e7703ec66728
v.yakovlev <wwevovancik@gmail.com>
2022-02-18 23:16:14 +0300

    Добавлена модель данных - Продукты

---
+++ AirplaneDelivery/BackEnd/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace BackEnd.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private DatabaseContext db;
+
+        public ProductController(DatabaseContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet("GetAllProducts")]
+        public List<Product> GetAllProducts()
+        {
+            return db.Products.ToList();
+        }
+    }
+}
--- AirplaneDelivery/BackEnd/Migrations/20220218201423_AddProducts.Designer.cs
+++ AirplaneDelivery/BackEnd/Migrations/20220218201423_AddProducts.Designer.cs
@@ -2,15 +2,17 @@
 using BackEnd;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace BackEnd.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20220218201423_AddProducts")]
+    partial class AddProducts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
