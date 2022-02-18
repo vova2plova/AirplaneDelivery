788c271681f31fbf983a93bae80540d3eee542d3
Александр Долгов <dolgov659@gmail.com>
2022-02-18 20:10:09 +0300

    1

--- AirplaneDelivery/DAL/Models/User.cs
+++ AirplaneDelivery/DAL/Models/User.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace DAL.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Password { get; set; }
--- AirplaneDelivery/FrontEnd/FrontEnd/ViewsModels/LoginPageViewModel .cs
+++ AirplaneDelivery/FrontEnd/FrontEnd/ViewsModels/LoginPageViewModel .cs
@@ -1,12 +1,32 @@
 ﻿using DAL.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace FrontEnd.ViewsModels
 {
     class LoginPageViewModel
     {
-        public User SignInUser (string login, string password)
+        const string url = "http://0e4a-176-52-96-105.ngrok.io/User/";
+        private HttpClient GetCLient()
         {
-            return null;
+            HttpClient client = new HttpClient();
+            return client;
+        }
+        public  async Task<User> SignInUser(string login, string password)
+        {
+            HttpClient client = GetCLient();
+            var result = await client.GetAsync(url + $"SignIn?login={login}&password={password}");
+            var json = await result.Content.ReadAsStringAsync();
+            var user = JsonConvert.DeserializeObject<User>(json);
+            if (user != null)
+            {
+                return user;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
