bc7ad71dcca68b358039b40302d7a3f39e3f6abe
Alexandr Dolgov <dolgov659@gmail.com>
2022-03-04 16:31:11 +0300

    Добавлен пакет Acr.UserDialogs

--- AirplaneDelivery/FrontEnd/FrontEnd.Android/MainActivity.cs
+++ AirplaneDelivery/FrontEnd/FrontEnd.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Acr.UserDialogs;
 
 namespace FrontEnd.Droid
 {
@@ -13,7 +14,7 @@ namespace FrontEnd.Droid
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
+            UserDialogs.Init(this);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
