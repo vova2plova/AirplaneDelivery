4d1d4ef89a101142a58cc6aaa95d98d8a06012fc
Alexandr Dolgov <dolgov659@gmail.com>
2022-03-17 00:15:31 +0300

    Добавил страницу  рецептов

---
+++ AirplaneDelivery/FrontEnd/FrontEnd/Views/DishesMainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FrontEnd.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DishesMainPage : ContentPage
+    {
+        public DishesMainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+        protected override void OnAppearing()
+        {
+
+            base.OnAppearing();
+        }
+     
+    }
+}
